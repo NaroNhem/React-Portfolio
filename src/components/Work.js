ea5aca6c159211d836b509443f51c9b9756030e1
Justin <n.justinaro@gmail.com>
2023-08-29 11:41:17 -0400

    changed colors of some text for better contrast

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,13 +4,13 @@ import Coffeeroasters from "../assets/Coffeeroasters.png";
 
 export default function Work() {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#474748]">
+    <div name="work" className="w-full md:h-screen text-white bg-[#474748]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-4xl font-bold inline border-b-4 border-[#CA2E55] text-gray-300 ">
+          <p className="text-4xl font-bold inline border-b-4 border-[#CA2E55] text-white">
             Work
           </p>
-          <p className="py-4 text-gray-300">Here are some of my recent work</p>
+          <p className="py-4 text-white">Here are some of my recent work</p>
         </div>
         {/*Card*/}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
